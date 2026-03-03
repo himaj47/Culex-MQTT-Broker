be9d6825ac7341882f83b45944056febbced4ec0
himaj47 <himajjoshi932@gmail.com>
2026-03-04 01:39:15 +0530

    ClientSession struct now keeps track of last activity and stores cleansession flag

--- mqtt/include/packethandler.h
+++ mqtt/include/packethandler.h
@@ -8,12 +8,14 @@ namespace culex {
 struct ClientSession {
     std::string client_id;
     uint16_t keep_alive{0};
+    bool cleansession{false};
     bool connected{false};
 
     std::unordered_set<std::string> subscriptions;
     std::weak_ptr<PacketHandler> session;
 
-    std::chrono::system_clock::time_point last_activity;
+    std::atomic<int64_t> expiry_ms{0};
+    std::atomic<int64_t> last_activity{0};
 
     // future:
     // inflight QoS1
