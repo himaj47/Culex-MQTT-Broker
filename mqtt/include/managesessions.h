8b29574be1bdf14d6bc3a439eb017bf8f55f98ad
himaj47 <himajjoshi932@gmail.com>
2026-03-04 01:33:28 +0530

    implemented MQTT keep alive feature
    + added struct ExpiryEntry for keeping track of expiry of client session
    + implemented a min heap to retrieve client session with least expiry period
    + implemented methods to schedule and process the disconnection after session expiration

--- mqtt/include/managesessions.h
+++ mqtt/include/managesessions.h
@@ -8,6 +8,15 @@
 
 namespace culex {
 
+struct ExpiryEntry {
+    int64_t expiry_ms;
+    std::weak_ptr<ClientSession> session;
+
+    bool operator>(const ExpiryEntry& other) const {
+        return expiry_ms > other.expiry_ms;
+    }
+};
+
 class ManageSessions : public SessionManager {
 public:
     ManageSessions(size_t pool_size);
@@ -19,11 +28,30 @@ public:
     std::shared_ptr<ClientSession> sessionPresent(std::string client_id);
     void addToRegistry(std::shared_ptr<ClientSession> cs);
     void removeFromRegistry(std::string client_id);
+    void updateActivity(std::string client_id);
+
+    void scheduleExpiry(std::shared_ptr<ClientSession> cs);
+    int getNextTimeoutMs();
+    void processExpiredSessions();
+
+    static int64_t now_ms() {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::steady_clock::now().time_since_epoch()
+        ).count();
+    }
 
 private:
     TopicTree m_topicTree;
     std::unordered_map<std::string, std::shared_ptr<ClientSession>> m_sessionRegistry;
+
+    std::priority_queue<
+        ExpiryEntry,
+        std::vector<ExpiryEntry>,
+        std::greater<ExpiryEntry>
+    > m_expiryHeap;
+
     std::mutex m_registryMutex;
+    std::mutex m_expiryMutex;
 };
 
 }
--- mqtt/src/managesession.cpp
+++ mqtt/src/managesession.cpp
@@ -10,7 +10,7 @@ ManageSessions::~ManageSessions() {
 }
 
 void ManageSessions::addSession(int fd) {
-    std::shared_ptr<PacketHandler> session = std::make_shared<PacketHandler>(fd, *this, m_executor, m_topicTree);
+    std::shared_ptr<PacketHandler> session = std::make_shared<PacketHandler>(fd, *this, *m_executor, m_topicTree);
     {
         std::unique_lock<std::shared_mutex> lock(m_sessionsRWMutex);
         m_sessions[fd] = std::move(session);
@@ -50,6 +50,90 @@ void ManageSessions::removeFromRegistry(std::string client_id) {
 void ManageSessions::addToRegistry(std::shared_ptr<ClientSession> cs) {
     std::lock_guard<std::mutex> lock(m_registryMutex);
     m_sessionRegistry[cs->client_id] = cs;
+}
+
+void ManageSessions::updateActivity(std::string client_id) {
+    if (auto cs = sessionPresent(client_id)) {
+        std::lock_guard<std::mutex> lock(m_registryMutex);
+        cs->last_activity.store(now_ms(), std::memory_order_relaxed);
+    }
+}
+
+void ManageSessions::scheduleExpiry(std::shared_ptr<ClientSession> cs) {
+    if (!cs->keep_alive) return;
+
+    int64_t expiry = cs->last_activity + (cs->keep_alive * 1500); // 1.5 * keep_alive, where keep_alive is in seconds
+    cs->expiry_ms.store(expiry, std::memory_order_relaxed);
+    
+    ExpiryEntry entry{expiry, cs};
+
+    std::lock_guard<std::mutex> lock(m_expiryMutex);
+    m_expiryHeap.push(std::move(entry));
+}
+
+int ManageSessions::getNextTimeoutMs() {
+    std::lock_guard<std::mutex> lock(m_expiryMutex);
+
+    if (m_expiryHeap.empty())
+        return -1; // block indefinitely
+
+    int64_t now = now_ms();
+    int64_t diff = m_expiryHeap.top().expiry_ms - now;
+
+    if (diff <= 0)
+        return 0;
+
+    return static_cast<int>(diff);
+}
+
+void ManageSessions::processExpiredSessions() {
+    int64_t now = now_ms();
+
+    std::vector<std::shared_ptr<ClientSession>> to_disconnect;
+    std::vector<std::shared_ptr<ClientSession>> new_entries;
+    {
+        std::lock_guard<std::mutex> lock(m_expiryMutex);
+
+        while (!m_expiryHeap.empty()) {
+            auto entry = m_expiryHeap.top();
+            int64_t recalculate_expiry = 0;
+
+            if (entry.expiry_ms > now)
+                break;
+
+            // do i need lock here?
+            if (auto cs = entry.session.lock()) {
+                recalculate_expiry = cs->last_activity + (cs->keep_alive * 1500);
+
+                if (recalculate_expiry > now) {
+                    new_entries.push_back(cs);
+                }
+
+                else {
+                    if (cs->connected)
+                        to_disconnect.push_back(cs);
+                }
+                
+                m_expiryHeap.pop();
+            }
+        }
+    }
+
+    for (auto& session : to_disconnect) {
+        if (auto transport = session->session.lock()) {
+            transport->forceDisconnect(); 
+
+            if (session->cleansession) {
+                removeFromRegistry(session->client_id);
+            } else {
+                session->connected = false;
+            }
+        }
+    }
+
+    for (auto& cs : new_entries) {
+        scheduleExpiry(cs);
+    }
 }
 
 }
