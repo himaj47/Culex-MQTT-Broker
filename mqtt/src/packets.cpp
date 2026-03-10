520439a5567a0d44a8d1e55639972064e94ea2fd
himaj47 <himajjoshi932@gmail.com>
2026-03-10 17:32:37 +0530

    - shift unpack handlers and build packet methods to handlers.h - fix: redefination error for encode and decode length methods

---
+++ mqtt/src/packets.cpp
@@ -0,0 +1,39 @@
+#include "packets.h"
+
+namespace culex {
+
+int encode_length(int len, std::vector<uint8_t>& buff) {
+    int encodedbyte = 0;
+
+    do {
+        if (encodedbyte + 1 > 4) return encodedbyte;
+
+        short d = len % 128;
+        len /= 128;
+        if (len > 0) d |= 128;
+
+        buff.push_back(d);
+
+    } while (len > 0);
+    return encodedbyte;
+}
+
+size_t decode_length(const uint8_t** buff) {
+    int len = 0;
+    int multiplier = 1;
+    uint8_t nextbyte = 0;
+
+    do {
+        nextbyte = **buff;
+        len += (nextbyte & 127) * multiplier;
+        multiplier *= 128;
+        
+        if (multiplier > 128*128*128) 
+            throw std::runtime_error("Malformed Remaining Length!!");
+
+        (*buff)++;
+    } while ((nextbyte & 128) != 0);
+    return len;
+}
+
+}
